5b227d275e8148130bd1a01cf5a0bea0195e64cd
Craig Lyons <craiglyons.dev@gmail.com>
2016-05-24 17:20:21 -0400

    Update karabiner settings

--- import_karabiner_settings.sh
+++ import_karabiner_settings.sh
@@ -2,15 +2,15 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set private.use_control_g_as_escape 1
-/bin/echo -n .
 $cli set option.emacsmode_ex_controlG 1
 /bin/echo -n .
 $cli set repeat.wait 40
 /bin/echo -n .
 $cli set repeat.initial_wait 233
 /bin/echo -n .
-$cli set option.emacsmode_ex_controlSpace 1
+$cli set option.emacsmode_ex_controlX 1
+/bin/echo -n .
+$cli set option.emacsmode_controlPNBF_ex 1
 /bin/echo -n .
 $cli set private.allow_b_f_with_arrow_keys 1
 /bin/echo -n .
