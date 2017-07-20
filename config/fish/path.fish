8dda713060456e33846ddde8388d2fe15a57cc03
Craig Lyons <craiglyons.dev@gmail.com>
2017-07-19 22:44:12 -0400

    Use code for git editor, remove escripts from path

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -2,5 +2,4 @@ set PATH ./node_modules/.bin \
          $PATH \
          /usr/local/bin \
          /usr/local/heroku/bin \
-	 /Users/craiglyons/.asdf/installs/elixir/1.4.2/.mix/escripts \
-         /Applications/Postgres.app/Contents/Versions/latest/bin
+	     /Applications/Postgres.app/Contents/Versions/latest/bin
